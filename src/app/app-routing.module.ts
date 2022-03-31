a89a6cbee2150ab4b40b8b34d4ff22d12300a3bf
Furkan-Erg <furkanerguldurenler@hotmail.com>
2022-04-01 01:15:52 +0300

    about-component-creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'about', component: AboutComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
