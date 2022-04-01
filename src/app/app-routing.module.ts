7841b4fbd3774f6e0296dc68f5a96ffcfa334a8a
Furkan-Erg <furkanerguldurenler@hotmail.com>
2022-04-01 14:33:03 +0300

    homepage-integration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about/about.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [{path: 'about', component: AboutComponent}];
+const routes: Routes = [{path: 'about', component: AboutComponent},{path:'home',component:HomeComponent},{path:'',redirectTo:'/home',pathMatch:'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
